0ff89287403d00a25a40510aeea720aedbc150b9
Natalie <natalie.ribero1@gmail.com>
2023-03-02 12:49:14 -0300

    Ingrese ejercicio 7

--- Condicionales/main.js
+++ Condicionales/main.js
@@ -105,13 +105,18 @@
 // }
 
 //Ejercicio 7 (Realizar un programa que permita el ingreso de los siguientes datos: Nombre, pase (vip o normal), si posee entrada (si o no, s o n, true o false). Las personas que posean tu mismo nombre tienen ingreso libre así como también los que posean un pase vip, mostrar un mensaje de bienvenida. Si posee entrada preguntar si desea utilizarla, en caso afirmativo mostrar mismo mensaje de bienvenida. Y por ultimo de no tener el mismo nombre o poseer un pase vip o entrada, preguntar si desea comprar, caso afirmativo solicitar dinero disponible, si posee $1000 o mas, mostrar mensaje de venta de entrada y bienvenida, caso contrario mostrar mensaje de rechazo de venta, y en caso contrario a no querer comprar, mostrar mensaje de despedida.)
-let nombreLibre = "Natalie"
 
 let nombre = prompt("Ingresa tu nombre");
 let pase = prompt("Introduzca su pase (“VIP”, “NORMAL” o “LIMITADO”)");
-let poseeEntrada = prompt("¿Posee entrada? SI/NO")
-let saldo = prompt("Introduzca su saldo");
+let poseeEntrada = prompt("¿Posee entrada? SI/NO");
 
+if ( nombre === "Natalie" || pase === "VIP"){
+    console.log("¡Bienvenido/a!");
+} else if(poseeEntrada=== "SI"){
+    console.log("¡Bienvenido/a!");
+} else {
+    let compraEntrada = prompt("¿Desea comprar una entrada? SI/NO")
+}
 
 
 //Ejercicio 8 (Realizar un juego de adivinanza estableciendo un valor entre 1 y 10 en una variable llamada numeroIncognita y que permita en 3 intentos adivinar el numero. El usuario deberá ingresar un numero del 1 al 10 por pantalla en 3 intentos el cual se deberá guardar en una variable llamada numeroIngresado, y en cada intento deberás mostrarle un mensaje al usuario diciendo: “el numero ingresado es mayor, vuelve a intentarlo” o “el numero ingresado es menor, vuelve a intentarlo” o en caso de adivinar, un mensaje que diga: “Ganaste, haz adivinado el numero.” No te preocupes si usas mucho código repetido, mas adelante veraz como realizar este juego de manera mas eficiente.)
